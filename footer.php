484e7c3b4bce9a378838249b6cb83de9ceeef347
muhrifat <muh.rifat2@gmail.com>
2017-05-22 16:02:54 +0700

    closes #2, closes #3 .. sampun rek, nama berubah jadi nextProject

---
+++ footer.php
@@ -0,0 +1,46 @@
+<div class="footer">
+	<div class="container">
+		<div class="col-md-3 footer-grids fgd1">
+		<a href="index.html"><img src="images/logo2.png" alt=" " /><h3>NEXT<span>project</span></h3></a>
+		<ul>
+			<li>Teknik Informatika, Lantai 3</li>
+			<li>Keputih, Sukolilo.</li>
+			<li><a href="mailto:info@example.com">info@example.com</a></li>
+			<a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+			<a href="#"><i class="fa fa-dribbble" aria-hidden="true"></i></a>
+			<a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+			<a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+		</ul>
+		</div>
+		<div class="col-md-3 footer-grids fgd2">
+			<h4>Information</h4>
+			<ul>
+				<li><a href="#">Contact Us</a></li>
+				<li><a href="#">Web Icons</a></li>
+				<li><a href="#">Typography</a></li>
+				<li><a href="#">FAQ's</a></li>
+			</ul>
+		</div>
+		<div class="col-md-3 footer-grids fgd3">
+			<h4>Shop</h4>
+			<ul>
+				<li><a href="#">Shirts</a></li>
+				<li><a href="#">Watchs</a></li>
+				<li><a href="#">Shoes</a></li>
+				<li><a href="#">Bags</a></li>
+			</ul>
+		</div>
+		<div class="col-md-3 footer-grids fgd4">
+			<h4>My Account</h4>
+			<ul>
+
+				<li><a href="#">Login</a></li>
+				<li><a href="#">Register</a></li>
+				<li><a href="#">Recommended </a></li>
+				<li><a href="#">Payments</a></li>
+			</ul>
+		</div>
+		<div class="clearfix"></div>
+		<p class="copy-right">© 2017 Nama olshop . All rights reserved | Design by <a href="http://w3layouts.com"> W3layouts.</a></p>
+	</div>
+</div>
--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $name = 'Nama Olshop';
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Situs Jual Beli Online Kekinian | Nama Olshop</title>
+<title>Situs Jual Beli Online Kekinian | nextProject</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="keywords" content="Situs jual beli kekinian untuk anak muda, jangan ngaku muda kalau belum beli di sini" />
@@ -31,100 +31,9 @@ $name = 'Nama Olshop';
 </head>
 
 <body>
-<div class="header-top-w3layouts">
-	<div class="container">
-		<div class="col-md-6 logo-w3">
-			<a href="index.html"><img src="images/logo2.png" alt=" " /><h1>Nama<span>Olshop</span></h1></a>
-		</div>
-		<div class="col-md-6 phone-w3l">
-			<ul>
-				<li><span class="glyphicon glyphicon-earphone" aria-hidden="true"></span></li>
-				<li>+6289631551715</li>
-			</ul>
-		</div>
-		<div class="clearfix"></div>
-	</div>
-</div>
-<div class="header-bottom-w3ls">
-	<div class="container">
-		<div class="col-md-7 navigation-agileits">
-			<nav class="navbar navbar-default">
-				<div class="navbar-header nav_2">
-					<button type="button" class="navbar-toggle collapsed navbar-toggle1" data-toggle="collapse" data-target="#bs-megadropdown-tabs">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-				</div>
-				<div class="collapse navbar-collapse" id="bs-megadropdown-tabs">
-					<ul class="nav navbar-nav ">
-						<li class=" active"><a href="index.html" class="hyper "><span>Home</span></a></li>
-						<li class="dropdown ">
-							<a href="#" class="dropdown-toggle  hyper" data-toggle="dropdown" ><span>Clothing<b class="caret"></b></span></a>
-								<ul class="dropdown-menu multi">
-									<div class="row">
-										<div class="col-sm-4">
-											<ul class="multi-column-dropdown">
-												<li><a href="#"><i class="fa fa-angle-right" aria-hidden="true"></i>Men's Clothing</a></li>
-												<li><a href="#"> <i class="fa fa-angle-right" aria-hidden="true"></i>Kid's Wear</a></li>
-												<li><a href="#"><i class="fa fa-angle-right" aria-hidden="true"></i>Party Wear</a></li>
 
-											</ul>
+<?php include('header.php') ?>
 
-										</div>
-										<div class="col-sm-4">
-											<ul class="multi-column-dropdown">
-												<li><a href="#"><i class="fa fa-angle-right" aria-hidden="true"></i>Casuals</a></li>
-												<li><a href="#"><i class="fa fa-angle-right" aria-hidden="true"></i>Night Wear</a></li>
-												<li><a href="#"><i class="fa fa-angle-right" aria-hidden="true"></i>Formals</a></li>
-												<li><a href="#"><i class="fa fa-angle-right" aria-hidden="true"></i>Inner Wear</a></li>
-											</ul>
-										</div>
-										<!-- <div class="clearfix"></div> -->
-									</div>
-								</ul>
-						</li>
-						<li><a href="about.html" class="hyper"><span>About</span></a></li>
-						<li><a href="contact.html" class="hyper"><span>Contact Us</span></a></li>
-					</ul>
-				</div>
-			</nav>
-		</div>
-								<script>
-				$(document).ready(function(){
-					$(".dropdown").hover(
-						function() {
-							$('.dropdown-menu', this).stop( true, true ).slideDown("fast");
-							$(this).toggleClass('open');
-						},
-						function() {
-							$('.dropdown-menu', this).stop( true, true ).slideUp("fast");
-							$(this).toggleClass('open');
-						}
-					);
-				});
-				</script>
-		<div class="col-md-4 search-agileinfo">
-			<form action="#" method="get">
-				<input type="search" name="Search" placeholder="Search for a Product..." required="">
-				<button type="submit" class="btn btn-default search" aria-label="Left Align">
-					<i class="fa fa-search" aria-hidden="true"> </i>
-				</button>
-			</form>
-		</div>
-		<div class="col-md-1 cart-wthree">
-			<div class="cart">
-				<form action="#" method="post" class="last">
-					<input type="hidden" name="cmd" value="_cart" />
-					<input type="hidden" name="display" value="1" />
-					<button class="w3view-cart" type="submit" name="submit" value=""><i class="fa fa-cart-arrow-down" aria-hidden="true"></i></button>
-				</form>
-			</div>
-		</div>
-		<div class="clearfix"></div>
-	</div>
-</div>
 <div class="banner-agile">
 	<!-- <div class="container">
 		<h2>WELCOME TO</h2>
@@ -1013,52 +922,7 @@ $name = 'Nama Olshop';
 <!-- newsletter -->
 
 <!-- //newsletter -->
-<div class="footer">
-	<div class="container">
-		<div class="col-md-3 footer-grids fgd1">
-		<a href="index.html"><img src="images/logo2.png" alt=" " /><h3>Nama<span>Olshop</span></h3></a>
-		<ul>
-			<li>Teknik Informatika, Lantai 3</li>
-			<li>Keputih, Sukolilo.</li>
-			<li><a href="mailto:info@example.com">info@example.com</a></li>
-			<a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-			<a href="#"><i class="fa fa-dribbble" aria-hidden="true"></i></a>
-			<a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-			<a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
-		</ul>
-		</div>
-		<div class="col-md-3 footer-grids fgd2">
-			<h4>Information</h4>
-			<ul>
-				<li><a href="#">Contact Us</a></li>
-				<li><a href="#">Web Icons</a></li>
-				<li><a href="#">Typography</a></li>
-				<li><a href="#">FAQ's</a></li>
-			</ul>
-		</div>
-		<div class="col-md-3 footer-grids fgd3">
-			<h4>Shop</h4>
-			<ul>
-				<li><a href="#">Shirts</a></li>
-				<li><a href="#">Watchs</a></li>
-				<li><a href="#">Shoes</a></li>
-				<li><a href="#">Bags</a></li>
-			</ul>
-		</div>
-		<div class="col-md-3 footer-grids fgd4">
-			<h4>My Account</h4>
-			<ul>
-
-				<li><a href="#">Login</a></li>
-				<li><a href="#">Register</a></li>
-				<li><a href="#">Recommended </a></li>
-				<li><a href="#">Payments</a></li>
-			</ul>
-		</div>
-		<div class="clearfix"></div>
-		<p class="copy-right">© 2017 Nama olshop . All rights reserved | Design by <a href="http://w3layouts.com"> W3layouts.</a></p>
-	</div>
-</div>
+<?php include('footer.php') ?>
 	<!-- cart-js -->
 	<script src="js/minicart.js"></script>
 	<script>
